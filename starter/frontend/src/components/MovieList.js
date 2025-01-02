9181ee104d8ee0c88ae5a3473ae639b6d96d40e4
phuongng8913 <phuongng8913@gmail.com>
2025-01-02 13:04:59 +0700

    Update MovieList.js

--- starter/frontend/src/components/MovieList.js
+++ starter/frontend/src/components/MovieList.js
@@ -6,7 +6,6 @@ function MovieList({ onMovieClick }) {
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
-    
     axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies`).then((response) => {
       setMovies(response.data.movies);
     });
